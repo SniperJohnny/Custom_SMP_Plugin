1e5d55947845fe9246dc3dcdc0f96ead2ffa60c4
SniperJohnny <johnjaekel2011@gmail.com>
2026-06-05 14:26:50 +0200

    init-commit

--- src/main/java/io/sniperjohnny/github/custom_SMP_Plugin/Custom_SMP_Plugin.java
+++ src/main/java/io/sniperjohnny/github/custom_SMP_Plugin/Custom_SMP_Plugin.java
@@ -5,22 +5,25 @@ import io.sniperjohnny.github.custom_SMP_Plugin.cuality_of_life.listeners.Join_l
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Custom_SMP_Plugin extends JavaPlugin {
+    public static Custom_SMP_Plugin instance = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
-
     saveDefaultConfig();
-
     getServer().getPluginManager().registerEvents(new Join_listener(this), this);
     getCommand("fly").setExecutor(new Fly_Command());
     getLogger().info("Custom_SMP_Plugin_started");
-
+    instance = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static final Custom_SMP_Plugin get_Instance() {
+        return instance;
     }
 }
